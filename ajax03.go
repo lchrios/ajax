20929ac8a7134b8922a3159089b82faf8366924c
Christopher Ortega <A01634500@itesm.mx>
2021-03-23 21:27:14 -0700

    init in heroku

--- ajax03.go
+++ ajax03.go
@@ -7,21 +7,21 @@ import (
 	"time"
 )
 
-func vuejs(w http.ResponseWriter, r *http.Request) {
+func vuejs03(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "vue01.html")
 }
 
 // Film is the struct containing film data
-type Film struct {
+type Film03 struct {
 	Title    string
 	Director string
 	Year     int
 }
 
-func sendFilms(w http.ResponseWriter, r *http.Request) {
+func sendFilms03(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	var films = [4]Film{
+	var films = [4]Film03{
 		{"No Manches Frida 2", "Nacho G. Velilla", 2016},
 		{"Star Wars 6", "George Lucas", 1976},
 		{"Harry Potter 3", "Alfonso Cuarón", 2002},
---
+++ go.mod
@@ -0,0 +1,3 @@
+module main.go/m/v2
+
+go 1.15
